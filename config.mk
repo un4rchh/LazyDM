a95154206693cc31decfafbc4ffa174397a217ae
un4rch <un4rch@example.com>
2026-06-10 23:12:02 +0300

    fix posix

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-CC = gcc
+CC = cc
 
 CFLAGS = -std=c11 -pedantic -Wall -Wextra -Werror -O2 -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -fstack-protector-strong -D_FORTIFY_SOURCE=2
 LDFLAGS = -lpam
